035e7c5897ead4c1fc615f51d2241b5597f833b4
DMelisena <demonlordx777@gmail.com>
2025-06-15 08:08:22 +0800

    Add minimum file
    minimum file for an app
    
    add .xcodeproj

---
+++ ReClockSync/ReClockSync/ContentView.swift
@@ -0,0 +1,24 @@
+@_exported import Inject
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject private var io = Inject.observer // swiftlint:disable:this identifier_name
+
+    var body: some View {
+        VStack {
+            Image(systemName: "apple.logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 100)
+                .imageScale(.large)
+                .foregroundStyle(.black)
+
+            Text("test")
+                .font(.largeTitle)
+                .fontDesign(.rounded)
+                .fontWeight(.bold)
+        }
+        .padding()
+        .enableInjection()
+    }
+}
---
+++ ReClockSync/ReClockSync/ReClockSync.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+@_exported import Inject
+
+@main
+struct ReClockSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
