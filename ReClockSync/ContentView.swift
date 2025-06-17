d1869fdd6e2e53b37fb9d1a9cc94fb839a611184
DMelisena <demonlordx777@gmail.com>
2025-06-17 10:32:11 +0800

    Add xcassets
    add preview xcassets
    
    update pbxproj

--- ReClockSync/ContentView.swift
+++ ReClockSync/ContentView.swift
@@ -3,9 +3,13 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject private var io = Inject.observer // swiftlint:disable:this identifier_name
+    private let defaults = UserDefaults.standard
+    var startValue = CGFloat(UserDefaults.standard.double(forKey: "startAngle"))
+    var endValue = CGFloat(UserDefaults.standard.double(forKey: "endAngle"))
 
     var body: some View {
         VStack {
+            ClockSlider(startAngle: startValue, endAngle: endValue)
             Image(systemName: "apple.logo")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
